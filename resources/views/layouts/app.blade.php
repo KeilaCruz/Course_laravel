14a9c6d30a040c7e53944d639043a86cdc190c7e
Keila <zS20019068@estudiantes.uv.mx>
2023-10-31 18:27:59 -0600

    View crear cuenta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,17 +5,21 @@
     <title>DevStagram-@yield('titulo')</title>
      @vite('resources/css/app.css') 
 </head> 
-<body> 
-    <nav>
-        <a href="/">Principal</a>
-        <a href="/nosotros">Nosotros</a>
-    <a href="/tienda">Tienda</a>
-    </nav>
-
-    <h1 class="text-4xl font-bold">@yield('titulo')</h1>
-
-    <hr>
-
-    @yield('contenido')
+<body class="bg-gray-100"> 
+    <header class="p-5 border-b bg-white shadow">
+      <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-3xl font-black">DevStagram</h1>
+        <nav class="flex gap-2 items-center">
+            <a class="font-bold uppercase text-gray-600" href="#">Login</a>
+            <a class="font-bold uppercase text-gray-600" href="/crear-cuenta">Crear cuenta</a>
+        </nav>
+        </div>   
+    </header>
+    <main class="container mx-auto mt-10">
+        <h2 class="font-black text-center text-xl mb-10">@yield('titulo')</h2>
+        @yield('contenido')</main>
+    <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
+        DevStagram- Todos los derechos reservados {{ now()->year}}
+    </footer>
 </body>
 </html>
--- resources/views/tienda.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('titulo')
-    Tienda
-@endsection
-
-@section('contenido')
-    Página de tienda
-@endsection
